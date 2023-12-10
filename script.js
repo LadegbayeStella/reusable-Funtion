6cfe35713524ab9117160347aeceb7f06d3b89b1
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-12-10 23:57:42 +0100

    power

---
+++ script.js
@@ -0,0 +1,16 @@
+// The power function calculates the result of raising value1 to the power of value2.
+// In this example, it computes and logs the result of 5^3 to the console.
+
+function power(value1, value2) {
+  return Math.pow(value1, value2);
+}
+console.log(power(5, 3));
+
+
+// function to Return the Remainder from Two Numbers
+
+function remainderOfTwo(value1, value2) {
+  return value1 % value2;
+}
+
+console.log(remainderOfTwo(21, 4));
